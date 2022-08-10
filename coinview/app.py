e09bfc8d5c737b7f92e1817f9ff06ec11fac411f
awesomer777 <aawesomer777@gmail.com>
2022-08-10 19:05:03 +0800

    test2

--- coinview/app.py
+++ coinview/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, flash, redirect, jsonify
-import config, csv, datetime
+import config
+import csv
+import datetime
 from binance.client import Client
 from binance.enums import *
 
 
-## this is a demo test
+# this is a demo test and test
 app = Flask(__name__)
-app.secret_key = b'somelongrandomstring'
+app.secret_key = bc'somelongrandomstring'
 
 client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
@@ -29,10 +31,10 @@ def index():
 def buy():
     print(request.form)
     try:
-        order = client.create_order(symbol=request.form['symbol'], 
-            side=SIDE_BUY,
-            type=ORDER_TYPE_MARKET,
-            quantity=request.form['quantity'])
+        order = client.create_order(symbol=request.form['symbol'],
+                                    side=SIDE_BUY,
+                                    type=ORDER_TYPE_MARKET,
+                                    quantity=request.form['quantity'])
     except Exception as e:
         flash(e.message, "error")
 
@@ -48,18 +50,20 @@ def sell():
 def settings():
     return 'settings'
 
+
 @app.route('/history')
 def history():
-    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "1 Jul, 2020", "12 Jul, 2020")
+    candlesticks = client.get_historical_klines(
+        "BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "1 Jul, 2020", "12 Jul, 2020")
 
     processed_candlesticks = []
 
     for data in candlesticks:
-        candlestick = { 
-            "time": data[0] / 1000, 
+        candlestick = {
+            "time": data[0] / 1000,
             "open": data[1],
-            "high": data[2], 
-            "low": data[3], 
+            "high": data[2],
+            "low": data[3],
             "close": data[4]
         }
 
